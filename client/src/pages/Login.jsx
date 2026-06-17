9b4ac4d9c59b1810798fe6a0b00bff4d2c9ba11d
CodeWithAman <amanverma78503@gmail.com>
2026-06-17 17:05:47 +0530

    feat: add Navbar and Login components with responsive design

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Deliveryboy from "../assets/deliberyboy.png";
+
+const Login = () => {
+  return (
+    <>
+      <div className=" h-[91.8vh] bg-linear-to-r from-(--secondary) to-(--primary) grid grid-cols-2 p-10 gap-20 items-center">
+        <div className=" hidden md:block">
+          <img src={Deliveryboy} alt="image" className=" rotate-y-180"/>
+        </div>
+
+        <div className=" w-md h-90 bg-(--background) rounded shadow p-10 flex flex-col justify-center">
+          <div className=" font-bold text-4xl text-center mb-5">Welcome Back!</div>
+
+          <form>
+            <div className=" flex flex-col gap-2">
+              <label htmlFor="email" className="font-semibold">Email</label>
+              <input type="email" name="email" id="email" className=" border p-2 rounded-xl focus:outline-none focus:ring-1 focus:ring-(--accent)"/>
+            </div>
+
+            <div className=" flex flex-col gap-2">
+              <label htmlFor="password" className="font-semibold">Password</label>
+              <input type="password" name="password" id="password" className=" border p-2 rounded-xl focus:outline-none focus:ring-1 focus:ring-(--accent)" />
+            </div>
+
+            <button type="submit" className="w-92 mt-6 bg-(--primary) text-white py-2 px-4 rounded-xl hover:bg-(--success)">Login</button>
+          </form>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
