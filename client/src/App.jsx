86008415236334e2454ac0f33a44d6629b5cbcf4
CodeWithAman <amanverma78503@gmail.com>
2026-07-05 20:39:23 +0530

    feat: update routing structure and improve component imports in App and main files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ContactUs from "./pages/ContactUs";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { Toaster } from "react-hot-toast";
-import UserDashborad from "./pages/dashboard/UserDashborad";
+import CustomerDashborad from "./pages/dashboard/CustomerDashborad";
 
 const App = () => {
   return (
@@ -19,10 +19,11 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/register/:userType" element={<Register />} />
           <Route path="/contact-Us" element={<ContactUs />} />
 
           {/* dashborad routes */}
-          <Route path="/user/dashboard" element={<UserDashborad/>} />
+          <Route path="/customer/dashboard" element={<CustomerDashborad/>} />
         </Routes>
         <Footer />
       </BrowserRouter>
