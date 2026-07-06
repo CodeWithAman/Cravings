75dfe99b9a7e431b8bc6360bee39083149b7b13e
CodeWithAman <amanverma78503@gmail.com>
2026-07-06 14:18:45 +0530

    feat: implement user authentication and profile management with registration, login, and logout functionality

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import { genToken } from "../utils/auth.service.js";
 
 export const RegisterUser = async (req, res, next) => {
   try {
@@ -19,8 +20,12 @@ export const RegisterUser = async (req, res, next) => {
       return next(error);
     }
 
+    const photoURL = `https://placehold.co/600x400?text=${fullname.charAt(0).toUpperCase()}`;
 
-    const photo = `https://placehold.co/600x400?text=${fullname.charAt(0).toUpperCase()}`;
+    const photo = {
+      url: photoURL,
+      publicId: null,
+    };
 
     const SALT = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, SALT);
@@ -67,6 +72,8 @@ export const LoginUser = async (req, res, next) => {
       return next(error);
     }
 
+    await genToken(existingUser, res);
+
     res.status(200).json({
       message: "User Login Sucessfully | Welcome Back !!",
       data: existingUser,
@@ -79,7 +86,9 @@ export const LoginUser = async (req, res, next) => {
 
 export const LogoutUser = async (req, res, next) => {
   try {
-    // controller logic
+    res.clearCookie("UserData", { maxAge: 0 });
+
+    res.status(200).json({ message: "Logout Sucessfully" });
   } catch (error) {
     console.log(error.message);
     next();
