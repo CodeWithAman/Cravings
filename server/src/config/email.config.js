64a41f70d2da5ced1cef191e9102fef39e772a17
CodeWithAman <amanverma78503@gmail.com>
2026-07-12 01:59:11 +0530

    feat: implement email sending functionality using nodemailer

---
+++ server/src/config/email.config.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (to, subject, message) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USERNAME || "amansen88127@gmail.com",
+        pass: process.env.GMAIL_PASSCODE || "pwforgwxoozsxctz",
+      },
+    });
+
+    const mailOption = {
+      from: process.env.GMAIL_USERNAME,
+      to,
+      subject,
+      html: message,
+    };
+
+    const res = await transporter.sendMail(mailOption);
+    console.log(res);
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+};
+
+export default sendEmail;
+
+// sendEmail(
+//     "amansen88127@gmail.com",
+//     "test Email",
+//     `<h1 style='color:blue;'>Text Message<h1/>
+//     <p style='color:red;'>Btach FSD 6</p>`,
+// );
