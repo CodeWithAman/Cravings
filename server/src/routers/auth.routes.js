0460c3cacf885d2381227387c83047d919f0be03
CodeWithAman <amanverma78503@gmail.com>
2026-07-12 01:58:54 +0530

    feat: add nodemailer dependency and implement OTP routes for user authentication

--- server/src/routers/auth.routes.js
+++ server/src/routers/auth.routes.js
@@ -3,7 +3,11 @@ import {
   RegisterUser,
   LoginUser,
   LogoutUser,
+  SendOTP,
+  VerifyOTP,
+  ResetPassword,
 } from "../controllers/auth.controller.js";
+import { OTPAuthProtect } from "../middlewares/auth.middleware.js";
 
 
 const router = express.Router()
@@ -11,5 +15,9 @@ const router = express.Router()
 router.post("/register", RegisterUser)
 router.post("/login", LoginUser)
 router.get("/logout", LogoutUser)
+
+router.post("/send-otp" , SendOTP)
+router.post("/verify-otp", VerifyOTP)
+router.post("/reset-password", OTPAuthProtect, ResetPassword)
 
 export default router;
