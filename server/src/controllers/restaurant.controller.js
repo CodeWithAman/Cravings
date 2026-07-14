fd866c997b9f3a761c7266dd9a66959ee72732a1
CodeWithAman <amanverma78503@gmail.com>
2026-07-14 12:42:30 +0530

    feat: implement restaurant profile update functionality with image uploads

---
+++ server/src/controllers/restaurant.controller.js
@@ -0,0 +1,101 @@
+import Restaurant from "../models/restaurant.model.js";
+import {
+  uploadMultipleImages,
+  deleteMultipleImages,
+  uploadSingleImage,
+  deleteSingleImage,
+} from "../utils/image.service.js";
+
+export const RestaurantUpdateProfile = async (req, res, next) => {
+  try {
+    const currentUser = req.user;
+    const restaurantData = req.body;
+    const coverImage = req.files?.coverImage;
+    const restaurantImages = req.files?.restaurantImages;
+
+    const dataKeys = Object.keys(restaurantData);
+
+    dataKeys.forEach((keys) => {
+      if (!restaurantData[keys]) {
+        const error = new Error(`Missing required field: ${keys}`);
+        error.statusCode = 400;
+        return next(error);
+      }
+    });
+
+    const existingRestaurant = await Restaurant.findOne({
+      managerId: currentUser._id,
+    });
+
+    if (!existingRestaurant) {
+      if (coverImage) {
+        const coverImage = await uploadSingleImage(
+          coverImage,
+          `restaurant/${currentUser.phone}/coverPhoto`,
+        );
+
+        dataKeys.push("coverImage");
+        restaurantData.coverImage = coverImage;
+      }
+
+      if (restaurantImages && restaurantImages.length > 0) {
+        const restaurantImage = await uploadMultipleImages(
+          restaurantData,
+          `restaurant/${currentUser.phone}/restaurantPhotos`,
+        );
+
+        dataKeys.push("restaurantImages");
+        restaurantData.restaurantImages = restaurantImage;
+      }
+
+      const newRestaurant = await Restaurant.create({
+        managerId: currentUser._id,
+        ...restaurantData,
+      });
+
+      return res.status(201).json({
+        message: "Restaurant Profile Created Successfully",
+        data: newRestaurant,
+      });
+    } else {
+      if (coverImage) {
+        await deleteSingleImage(existingRestaurant.coverImage);
+
+        const coverImage = await uploadSingleImage(
+          coverImage,
+          `restaurant/${currentUser.phone}/coverPhoto`,
+        );
+
+        dataKeys.push("coverImage");
+        restaurantData.coverImage = coverImage;
+      }
+
+      if (restaurantImages && restaurantImages.length > 0) {
+        await deleteMultipleImages(existingRestaurant.restaurantImage);
+
+        const restaurantImage = await uploadMultipleImages(
+          restaurantData,
+          `restaurant/${currentUser.phone}/restaurantPhotos`,
+        );
+
+        dataKeys.push("restaurantImages");
+        restaurantData.restaurantImages = restaurantImage;
+      }
+
+      dataKeys.forEach((keys) => {
+        existingRestaurant[keys] =
+          restaurantData[keys] || existingRestaurant[keys];
+      });
+
+      await existingRestaurant.save();
+
+      return res.status(200).json({
+        message: "Restaurant Profile Updated Successfully",
+        data: existingRestaurant,
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    next();
+  }
+};
