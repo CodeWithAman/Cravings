4e3e25f0fb8514a5aa47f3ccd033a4731fad6fed
CodeWithAman <amanverma78503@gmail.com>
2026-07-16 17:27:02 +0530

    fix: adjust height of dashboard components for consistent layout

--- client/src/pages/dashboard/RestaurantDashboard.jsx
+++ client/src/pages/dashboard/RestaurantDashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation , useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import RestaurantSidebar from "../../components/restaurantDashboard/RestaurantSidebar";
 import RestaurantOverview from "../../components/restaurantDashboard/RestaurantOverview";
@@ -7,7 +7,7 @@ import RestaurantOrders from "../../components/restaurantDashboard/RestaurantOrd
 import RestaurantSettings from "../../components/restaurantDashboard/RestaurantSettings";
 
 const RestaurantDashboard = () => {
-  const { isLogin , role } = useAuth();
+  const { isLogin, role } = useAuth();
   const navigate = useNavigate();
   const active = useLocation().state?.activeTab;
   const [activeTab, setActiveTab] = React.useState(active || "overview");
@@ -17,8 +17,7 @@ const RestaurantDashboard = () => {
       <div className="h-[92vh] bg-[url('/foodTable.webp')]  bg-cover bg-center">
         <div className="h-full backdrop-blur-lg flex flex-col items-center justify-center ">
           <h1 className="text-2xl font-bold text-(--color-neutral-content)">
-            Access Denied. Please log in as a customer to view this
-            page.
+            Access Denied. Please log in as a customer to view this page.
           </h1>
           <button
             className="mt-4 px-4 py-2 bg-(--color-primary) text-white rounded-md"
@@ -31,15 +30,17 @@ const RestaurantDashboard = () => {
     );
   }
 
-
   return (
     <>
-      <div className="h-[92vh] flex gap-2 m-2">
+      <div className="h-[91vh] flex gap-2 m-2">
         <div className="w-3/17 bg-(--color-base-200) p-4 rounded-lg shadow-md h-full">
-          <RestaurantSidebar activeTab={activeTab} setActiveTab={setActiveTab} />
+          <RestaurantSidebar
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
         </div>
         <div className="w-14/17 bg-(--color-base-100) p-4 rounded-lg shadow-md h-full">
-          {activeTab === "overview" && <RestaurantOverview/>}
+          {activeTab === "overview" && <RestaurantOverview />}
           {activeTab === "orders" && <RestaurantOrders />}
           {activeTab === "settings" && <RestaurantSettings />}
         </div>
