b70477df3ad1b8b5aec7416fd3f0d6dc7ead8e67
CodeWithAman <amanverma78503@gmail.com>
2026-08-08 23:22:03 +0530

    feat: enhance Navbar with cart functionality and update logout icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { AiOutlineLogout } from "react-icons/ai";
+import logoLight from "../assets/transparentLogoLight.png";
 import { useAuth } from "../context/AuthContext";
-import navLogo from "../assets/logo-light.png";
-import api from "../config/api.config.js";
+import { useCart } from "../context/CartContext";
+import { FaPowerOff } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
 import toast from "react-hot-toast";
+import api from "../config/api.config.js";
 
 const Navbar = () => {
-  const { user, setUser, isLogin, setIsLogin, role, setRole } = useAuth();
+  const { user, isLogin, role, setUser, setIsLogin, setRole } = useAuth();
+  const { totalItems } = useCart();
   const navigate = useNavigate();
 
   const handleNavigate = () => {
+    //console.log("Handle Navigate", role);
     if (role === "restaurant") {
       navigate("/restaurant/dashboard");
     } else if (role === "rider") {
@@ -28,14 +32,14 @@ const Navbar = () => {
       toast.success(res.data.message);
 
       sessionStorage.removeItem("cravingUser");
-      setUser(false);
+      setUser(null);
       setIsLogin(false);
-      setRole?.(null);
+      setRole(null);
       navigate("/");
     } catch (error) {
       toast.error(
         error.response?.data?.message ||
-        "Something went wrong while logging out.",
+          "Unknown error occurred during registration. Please try again.",
       );
     }
   };
@@ -45,12 +49,26 @@ const Navbar = () => {
       <div className="sticky top-0 z-99 flex items-center justify-between px-12 py-1 bg-(--color-primary) text-white w-full h-16 shadow-md">
         <div className="h-full">
           <Link to="/">
-            <img src={navLogo} alt="Logo" className="w-fit h-full" />{" "}
+            <img src={logoLight} alt="Logo" className="w-fit h-full" />{" "}
           </Link>
         </div>
 
         {isLogin ? (
           <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 relative">
+              <button
+                onClick={() => navigate("/cart")}
+                className="hover:scale-110 transition-transform duration-200"
+                title="Go to Cart"
+              >
+                <IoCartOutline className="text-(--color-primary-content) text-3xl" />
+              </button>
+              {totalItems > 0 && (
+                <span className="absolute -top-2 -right-2 bg-(--color-error) text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
+                  {totalItems}
+                </span>
+              )}
+            </div>
             <button
               className="flex gap-2 items-center text-(--color-primary-content) border border-transparent hover:border-(--color-primary-content)  px-3 py-1 rounded"
               title="Go to Dashboard"
@@ -63,7 +81,7 @@ const Navbar = () => {
               />
               <div className="flex flex-col items-start">
                 <span className="text-base">{user?.fullname}</span>
-                <span className="text-xs text-(--color-primary-content)/80 uppercase font-semibold">
+                <span className="text-xs text-(--color-primary-content)/80 uppercase">
                   {role}
                 </span>
               </div>
@@ -73,7 +91,7 @@ const Navbar = () => {
               className="text-(--color-primary-content) border border-transparent hover:border-(--color-primary-content) hover:bg-(--color-error) px-3 py-3 rounded"
               title="Logout"
             >
-              <AiOutlineLogout />
+              <FaPowerOff />
             </button>
           </div>
         ) : (
